36b6a582d6116ef137cf9010f4d6a00604d3bc1d
Dimitri Glazkov <dglazkov@chromium.org>
2015-02-25 15:35:26 -0800

    Added html gulp task, factoring away as-html.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,32 @@
 var gulp = require('gulp');
 var parseArgs = require('minimist');
+var fs = require('fs');
+
+var TreeBuilder = require('./lib/tree-builder.js');
+var HTMLWriter = require('./lib/html-writer.js');
 
 var options = parseArgs(process.argv.slice(2));
 
 gulp.task('default', function() {
-  console.log('Hello world!', options);
+  console.log('Hello world!');
 });
+
+gulp.task('html', function(cb) {
+  var name = options.file;
+
+  fs.readFile(name, 'utf8', function(err, data) {
+    if (err)
+      throw err;
+
+    var data = JSON.parse(data);
+
+    var writer = new HTMLWriter();
+    var builder = new TreeBuilder(writer);
+    builder.build(data);
+    builder.write(writer);
+
+    console.log(writer.getHTML());
+    cb();
+  });
+
+})
