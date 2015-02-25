1194d7c26e583b047591d272f25351ba097e525c
Dimitri Glazkov <dglazkov@chromium.org>
2015-02-25 15:09:08 -0800

    Add minimist.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var parseArgs = require('minimist');
+
+var options = parseArgs(process.argv.slice(2));
 
 gulp.task('default', function() {
-  console.log('Hello world!');
+  console.log('Hello world!', options);
 });
