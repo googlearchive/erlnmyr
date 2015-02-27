867338dfdc1450622ff105a1f42b755780190c0c
Dimitri Glazkov <dglazkov@chromium.org>
2015-02-26 16:06:10 -0800

    Unfinished js writer.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var parseArgs = require('minimist');
 var fs = require('fs');
 
-var TreeBuilder = require('./lib/tree-builder.js');
-var HTMLWriter = require('./lib/html-writer.js');
+var TreeBuilder = require('./lib/tree-builder');
+var HTMLWriter = require('./lib/html-writer');
+var JSWriter = require('./lib/js-writer');
 
 var options = parseArgs(process.argv.slice(2));
 
@@ -29,4 +30,30 @@ gulp.task('html', function(cb) {
     cb();
   });
 
-})
+});
+
+gulp.task('js', function(cb) {
+  var name = options.file;
+
+  fs.readFile(name, 'utf8', function(err, data) {
+    if (err)
+      throw err;
+
+    var data = JSON.parse(data);
+
+    var writer = new JSWriter();
+    var builder = new TreeBuilder(writer);
+    builder.build(data);
+    builder.write(writer);
+
+    var output = 'result.html';
+
+    fs.writeFile(output, writer.getHTML(), function(err) {
+      if (err)
+        throw err;
+      console.log('written results into \"' + output + '\".');
+      cb();
+    })
+  });
+
+});
