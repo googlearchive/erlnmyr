7073f5bac79532e954858ca431ad187f8d98b300
Shane Stephens <shans@chromium.org>
2015-06-01 11:09:26 +1000

    Graph basic infrastructure.

---
+++ core/graph.js
@@ -0,0 +1,194 @@
+var assert = require('chai').assert;
+
+var _id = 0;
+
+function newID(type) {
+  return type + (_id++);
+}
+
+function Connection() {
+  this.graph = undefined;
+  this.fromPipes = [];
+  this.toPipes = [];
+  this.id = newID('n');
+}
+
+Connection.prototype.intoPipe = function(pipe) {
+  assert(pipe.in == undefined);
+  mergeGraphs(this, pipe);
+  this.toPipes.push(pipe);
+  pipe.in = this;
+}
+
+Connection.prototype.fromPipe = function(pipe) {
+  assert(pipe.out == undefined);
+  mergeGraphs(this, pipe);
+  this.fromPipes.push(pipe);
+  pipe.out = this;
+}
+
+Connection.prototype.isPipe = function() { return false; }
+Connection.prototype.isConnection = function() { return true; }
+
+
+function Pipe(stageName, options) {
+  this.stageName = stageName;
+  this.options = options || {};
+  this.in = undefined;
+  this.out = undefined;
+  this.graph = undefined;
+  this.id = newID('e');
+}
+
+Pipe.prototype.isPipe = function() { return true; }
+Pipe.prototype.isConnection = function() { return false; }
+
+Pipe.prototype.from = function(connections) {
+  if (typeof(connections.length) !== 'number')
+    connections = [connections];
+
+  for (var i = 0; i < connections.length; i++)
+    connect(connections[i], this);
+
+  if (this.out == undefined) {
+    var connection = new Connection();
+    connect(this, connection);
+  }
+  return this.out;
+}
+
+function Graph() {
+  this.nodes = {};
+  this.edges = {};
+}
+
+Graph.prototype.addEdge = function(edge) {
+  if (edge == undefined || this.edges[edge.id] == edge)
+    return;
+  assert(this.edges[edge.id] == undefined);
+  this.edges[edge.id] = edge;
+  edge.graph = this;
+}   
+
+Graph.prototype.addNode = function(node) {
+  if (node == undefined || this.nodes[node.id] == node)
+    return;
+  assert(this.nodes[node.id] == undefined);
+  this.nodes[node.id] = node;
+  node.graph = this;
+}
+
+Graph.prototype.merge = function(graph) {
+  for (node in graph.nodes) {
+    assert(this.nodes[node] == undefined);
+    this.nodes[node] = graph.nodes[node];
+    this.nodes[node].graph = this;
+  }
+  for (edge in graph.edges) {
+    assert(this.edges[edge] == undefined);
+    this.edges[edge] = graph.edges[edge];
+    this.edges[edge].graph = this;
+  }
+  graph.nodes = {};
+  graph.edges = {};
+}
+
+Graph.prototype.contains = function(a) {
+  if (a.isConnection())
+    return this.nodes[a.id] == a;
+  else
+    return this.edges[a.id] == a;
+}
+
+function mergeGraphs(a, b) {
+  if (a.graph == b.graph && a.graph !== undefined)
+    return;
+
+  var updateBothSides = false;
+  if (a.graph == undefined && b.graph == undefined) {
+    a.graph = new Graph();
+    b.graph = a.graph;
+    updateBothSides = true;
+  } else if (a.graph != undefined && b.graph != undefined && a.graph != b.graph) {
+    a.graph.merge(b.graph);
+  } 
+
+  if (a.graph == undefined || updateBothSides) {
+    if (a.isPipe())
+      b.graph.addEdge(a);
+    else 
+      b.graph.addNode(a);
+  }
+  if (b.graph == undefined || updateBothSides) {
+    if (b.isPipe())
+      a.graph.addEdge(b);
+    else
+      a.graph.addNode(b);
+  }
+}
+
+function connectPipes(pipeWithOut, pipeWithIn) {
+  assert(pipeWithIn.isPipe());
+  assert(pipeWithOut.isPipe());
+
+  mergeGraphs(pipeWithOut, pipeWithIn);
+  if (pipeWithOut.out == undefined && pipeWithIn.in == undefined) {
+    var connection = new Connection();
+    connection.fromPipe(pipeWithOut);
+    connection.intoPipe(pipeWithIn);
+  } else if (pipeWithOut.out == undefined) {
+    pipeWithIn.in.fromPipe(pipeWithOut);
+  } else if (pipeWithIn.in == undefined) {
+    pipeWithOut.out.intoPipe(pipeWithIn);
+  } else if (pipeWithOut.out !== pipeWithIn.in) {
+    var edge = new Pipe();
+    connectPipes(pipeWithOut, edge);
+    connectPipes(edge, pipeWithIn);
+  }
+}
+
+function connectToPipe(connection, pipeWithIn) {
+  assert(connection.isConnection());
+  assert(pipeWithIn.isPipe());
+
+  mergeGraphs(connection, pipeWithIn);
+  if (pipeWithIn.in == undefined) {
+    connection.intoPipe(pipeWithIn);
+  } else {
+    var edge = new Pipe();
+    connection.intoPipe(edge);
+    connectPipes(edge, pipeWithIn);
+  }
+}
+
+function connectFromPipe(pipeWithOut, connection) {
+  assert(connection.isConnection());
+  assert(pipeWithOut.isPipe());
+
+  mergeGraphs(pipeWithOut, connection);
+  if (pipeWithOut.out == undefined) {
+    connection.fromPipe(pipeWithOut);
+  } else {
+    var edge = new Pipe();
+    connectPipes(pipeWithOut, edge);
+    connection.fromPipe(edge);
+  }
+}
+
+function connect(a, b) {
+  if (a.isPipe() && b.isPipe()) {
+    connectPipes(a, b);
+  } else if (a.isPipe() && b.isConnection()) {
+    connectFromPipe(a, b);
+  } else if (a.isConnection() && b.isPipe()) {
+    connectToPipe(a, b);
+  } else {
+    var edge = new Pipe();
+    connectToPipe(a, edge);
+    connectFromPipe(edge, b);
+  }
+}
+    
+module.exports.Pipe = Pipe;
+module.exports.Connection = Connection;
+module.exports.connect = connect;
---
+++ tests/graph.js
@@ -0,0 +1,115 @@
+var assert = require('chai').assert;
+
+var graph = require('../core/graph.js');
+
+function reachable(a, b) {
+  var reachableSet = {};
+  if (a.isPipe())
+    var delta = [a];
+  else
+    var delta = a.toPipes;
+
+  while (delta.length > 0) {
+    var newDelta = [];
+    for (var i = 0; i < delta.length; i++) {
+      if (b.isPipe() && delta[i] == b)
+        return true;
+      if (b.isConnection() && delta[i].out == b)
+        return true;
+      reachableSet[delta[i].id] = delta[i];
+    }
+    for (var i = 0; i < delta.length; i++) {
+      var pipe = delta[i];
+      if (pipe.out == undefined)
+        continue;
+      for (var i = 0; i < pipe.out.toPipes.length; i++) {
+        var outPipe = pipe.out.toPipes[i];
+        if (outPipe.id in reachableSet)
+          continue;
+        newDelta.push(outPipe);
+      }
+    }
+    delta = newDelta;
+  }
+  return false;
+}
+
+function assertConnected(a, b) {
+  assert.equal(a.graph, b.graph);
+  assert(a.graph.contains(a), "left argument is not in graph");
+  assert(a.graph.contains(b), "right argument is not in graph");
+  assert(reachable(a, b), "pipes are not connected");
+}
+  
+describe('graph.connect', function() {
+  it('should allow two pipes to connect', function() {
+    var a = new graph.Pipe('a');
+    var b = new graph.Pipe('b');
+    graph.connect(a, b);
+    assertConnected(a, b);
+  });
+  it('should allow a connection to connect to a pipe', function() {
+    var a = new graph.Connection();
+    var b = new graph.Pipe('b');
+    graph.connect(a, b);
+    assertConnected(a, b);
+  });
+  it('should allow a pipe to connect to a connection', function() {
+    var a = new graph.Pipe('a');
+    var b = new graph.Connection();
+    graph.connect(a, b);
+    assertConnected(a, b);
+  });
+
+  /*
+   * -a-> . -b->
+   *      |
+   *      v
+   * -c-> . -d->
+   */
+  it('should connect to already connected pipes', function() {
+    var a = new graph.Pipe('a');
+    var b = new graph.Pipe('b');
+    graph.connect(a, b);
+    var c = new graph.Pipe('c');
+    var d = new graph.Pipe('d');
+    graph.connect(c, d);
+    graph.connect(a, d);
+    assertConnected(a, b);
+    assertConnected(c, d);
+    assertConnected(a, d);
+    assert(!reachable(a, c), "c should not be reachable from a");
+    assert(!reachable(c, b), "b should not be reachable from c");
+  });
+});
+
+describe('Pipe.from', function() {
+  it('should handle a moderately complicated example', function() {
+    var a = new graph.Pipe('a');
+    var b = new graph.Pipe('b');
+    var c = new graph.Pipe('c');
+    var d = new graph.Pipe('d');
+    var e = new graph.Pipe('e');
+
+    function makeExample(input) {
+      var aout = a.from(input);
+      assertConnected(a, aout);
+      var bout = b.from(aout);
+      var cout = c.from(bout);
+      var dout = d.from(cout);
+      assertConnected(a, dout);
+      var result = e.from([aout, bout, cout, dout]);
+      assert(aout.toPipes.length == 2);
+      assert(bout.toPipes.length == 2);
+      assert(cout.toPipes.length == 2);
+      assert(dout.toPipes.length == 1);
+      assert(e.in.fromPipes.length == 4);
+      return result;
+    }
+
+    var con = new graph.Connection();
+    var out = makeExample(con);
+
+    assertConnected(con, out);
+  });
+});
