c31a3677e5b63bc5b7fa003924615d6b957aa52e
Douglas Stockwell <dstockwell@chromium.org>
2015-07-09 23:13:45 +1000

    Avoid failing on cyclic dependency

--- exports.js
+++ exports.js
@@ -1,2 +1,4 @@
+// TODO: Fix the cyclic dependency so this isn't needed.
+require('./core/stage-loader');
 module.exports.phase = require('./core/phase-register');
 module.exports.types = require('./core/types');
